c55927b1a14c20192c845135050c5f4a724ccf6c
Yihun Alemayehu <yankure01@gmail.com>
2023-11-24 12:09:08 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/breadcrumb/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.breadcrumb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
